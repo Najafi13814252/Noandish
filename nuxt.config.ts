08a0af81944a5c4e0e3688520196b9de5f7e8445
ahnajafi4252 <ahnajafi4252@gmail.com>
2025-10-02 17:17:35 +0330

    A part of the Navbar has been complated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/image', '@nuxt/icon'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: 'rtl'
+      },
+    }
   }
 })
